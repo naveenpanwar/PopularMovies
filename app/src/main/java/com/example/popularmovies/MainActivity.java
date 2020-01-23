36730cdd98dc9192699c4e25007fe31a24ba3b2a
Naveen Panwar <naveen.panwar27@gmail.com>
2020-01-23 14:45:24 +0530

    Initial Commit Added a Dummy Adapter and made some changes in the initial main_activity.xml file

---
+++ app/src/main/java/com/example/popularmovies/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.popularmovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mPopularMoviesRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/popularmovies/MoviesAdapter.java
@@ -0,0 +1,9 @@
+package com.example.popularmovies;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MoviesAdapter {
+}
